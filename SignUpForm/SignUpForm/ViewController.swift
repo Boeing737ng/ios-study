c0e26c23149f71dd2062ef21415435e2578e80c5
Boeing737ng <ghlove0317@naver.com>
2018-06-30 16:59:35 +0900

    Basic signup ui created

---
+++ SignUpForm/SignUpForm/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SignUpForm
+//
+//  Created by Kihyun Choi on 2018. 6. 30..
+//  Copyright © 2018년 sfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var pwdField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var blogField: UITextField!
+    @IBOutlet weak var resultTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func signUpAction(_ sender: Any) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
