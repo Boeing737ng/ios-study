a882a53f309c97fb471b2bacc1ddf1f58dd6ee09
Boeing737ng <ghlove0317@naver.com>
2018-07-01 23:04:15 +0900

    function explanation comment added

--- AppLifeCycle/AppLifeCycle/AppDelegate.swift
+++ AppLifeCycle/AppLifeCycle/AppDelegate.swift
@@ -49,7 +49,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         print("applicationDidBecomeActive")
     }
-
+    
+    //앱이 background에 간 후에 종료시키면 호출되지 않음 (가능하면 didEnterBackground에 필요 코드 작성)
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         print("applicationWillTerminate")
