d4571cc56e37924e8f716143c86acd466b95048f
Boeing737ng <ghlove0317@naver.com>
2018-07-01 22:54:38 +0900

    App life cycle checking project

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Kihyun Choi on 2018. 7. 1..
+//  Copyright © 2018년 sfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
