046464ef6eaa2bc82c2e454477190f78c60419bf
Boeing737ng <ghlove0317@naver.com>
2018-07-02 17:29:03 +0900

    datePicker and return info display changes

--- FlightBooking/FlightBooking/ViewController.swift
+++ FlightBooking/FlightBooking/ViewController.swift
@@ -17,12 +17,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        returnDateLabel.isHidden = true
+        returnDateButton.isHidden = true
+        datePicker.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func showDatePicker(_ sender: Any) {
-        
+        if datePicker.isHidden == false{
+            datePicker.isHidden = true
+        } else {
+            datePicker.isHidden = false
+        }
     }
     @IBAction func showReturnDate(_ sender: Any) {
+        if returnDateButton.isHidden == false{
+            returnDateLabel.isHidden = true
+            returnDateButton.isHidden = true
+        } else {
+            returnDateLabel.isHidden = false
+            returnDateButton.isHidden = false
+        }
     }
     @IBAction func selectDate(_ sender: Any) {
     }
