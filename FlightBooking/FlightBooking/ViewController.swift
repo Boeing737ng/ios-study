6ed137956783a1b6284d5e30ad20db6fa25388dd
Boeing737ng <ghlove0317@naver.com>
2018-07-02 17:21:23 +0900

    Required actionc functions and IBOutlet created

--- FlightBooking/FlightBooking/ViewController.swift
+++ FlightBooking/FlightBooking/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var departureDateButton: UIButton!
+    @IBOutlet weak var returnDateButton: UIButton!
+    @IBOutlet weak var returnDateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func showDatePicker(_ sender: Any) {
+        
+    }
+    @IBAction func showReturnDate(_ sender: Any) {
+    }
+    @IBAction func selectDate(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
