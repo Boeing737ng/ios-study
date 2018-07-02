62b980d0b93c446e4b955b916160e8bea70b86e6
Boeing737ng <ghlove0317@naver.com>
2018-07-02 14:31:10 +0900

    Required action functions and IBOutlet created

--- MiniBrowser/MiniBrowser/ViewController.swift
+++ MiniBrowser/MiniBrowser/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bookMarkSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var mainWebView: WKWebView!
+    @IBOutlet weak var spinningActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func bookMarkAction(_ sender: Any) {
+    }
+    
+    @IBAction func goBackAction(_ sender: Any) {
+    }
+    
+    @IBAction func goForwardAction(_ sender: Any) {
+    }
+    
+    @IBAction func stopLoadingAction(_ sender: Any) {
+    }
+    
+    @IBAction func reloadAction(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
